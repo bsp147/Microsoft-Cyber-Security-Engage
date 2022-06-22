2e173752b4d0572549cacd1a2ee12937f26f039a
Bijay <bijaysingh0147@gmail.com>
2022-06-22 22:04:44 +0530

    loop over ports

--- PortScan.py
+++ PortScan.py
@@ -2,15 +2,14 @@
 
 from scapy.all import *
 
-ports = [25,80,53,443,445,8080,8443]
-
 
 def SynScan(host):
-	ans,unans = sr(IP(dst = host)/TCP(sport=5555,dport=ports,flags="S"),timeout=2,verbose=0)
-	print("Open ports at %s:"%host)
-	for(s,r,) in ans:
-		if(s[TCP].dport == r[TCP].sport):
-			print(s[TCP].dport)
+	for ports in range(1,65535):
+		ans,unans = sr(IP(dst = host)/TCP(sport=5555,dport=ports,flags="S"),timeout=2,verbose=0)
+		print("Open ports at %s:"%host)
+		for(s,r,) in ans:
+			if(s[TCP].dport == r[TCP].sport):
+				print(s[TCP].dport)
 
 
 def DNSScan(host):
